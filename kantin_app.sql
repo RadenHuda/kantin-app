a842d98b988fd2d2e448aea923bbc84e143e88b5
RadenHuda <78721284+RadenHuda@users.noreply.github.com>
2025-07-14 10:47:33 +0700

    tambah database
    update database kantin-app

---
+++ kantin_app.sql
@@ -0,0 +1,83 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 14 Jul 2025 pada 05.45
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kantin_app`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `daftar_kelas`
+--
+-- Kesalahan membaca struktur untuk tabel kantin_app.daftar_kelas: #1932 - Table &#039;kantin_app.daftar_kelas&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel kantin_app.daftar_kelas: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kantin_app`.`daftar_kelas`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `daftar_menus`
+--
+-- Kesalahan membaca struktur untuk tabel kantin_app.daftar_menus: #1932 - Table &#039;kantin_app.daftar_menus&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel kantin_app.daftar_menus: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kantin_app`.`daftar_menus`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `daftar_murids`
+--
+-- Kesalahan membaca struktur untuk tabel kantin_app.daftar_murids: #1932 - Table &#039;kantin_app.daftar_murids&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel kantin_app.daftar_murids: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kantin_app`.`daftar_murids`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `ordermenus`
+--
+-- Kesalahan membaca struktur untuk tabel kantin_app.ordermenus: #1932 - Table &#039;kantin_app.ordermenus&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel kantin_app.ordermenus: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kantin_app`.`ordermenus`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+-- Kesalahan membaca struktur untuk tabel kantin_app.users: #1932 - Table &#039;kantin_app.users&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel kantin_app.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kantin_app`.`users`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `wali_kelas`
+--
+-- Kesalahan membaca struktur untuk tabel kantin_app.wali_kelas: #1932 - Table &#039;kantin_app.wali_kelas&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel kantin_app.wali_kelas: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kantin_app`.`wali_kelas`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `wali_murids`
+--
+-- Kesalahan membaca struktur untuk tabel kantin_app.wali_murids: #1932 - Table &#039;kantin_app.wali_murids&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel kantin_app.wali_murids: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kantin_app`.`wali_murids`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
